c38f5a9e3b8cab9c32e92bd30eebc7a5b35acedc
Mykhailo Kysliuk <mkysliuk@mapr.com>
2019-02-18 22:58:28 +0200

    Some intro added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,66 +1,20 @@
-/*
-$(document).ready(function () {
 
-    /!* For the sticky navigation *!/
-    $('.js--section-features').waypoint(function (direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            this.offset = '300px';
-            $('nav').removeClass('sticky');
-
+if (!hasTouch()) {
+    document.body.className += ' hasHover';
+} else {
+    document.querySelector('.navigation-list').addEventListener('click', event => {
+        const navItem = event.target.closest('.navigation-item');
+        if (navItem) {
+            navItem.classList.toggle('dropdown-opened');
         }
-    }, { offset: '60px' });
+    })
+}
 
-     /!* For the scrolling *!/
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
-
-    /!* Animations on scroll *!/
-
-    $('.js--wp-1').waypoint(function (direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, { offset: '50%' });
-
-    $('.js--wp-2').waypoint(function (direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, { offset: '50%' });
-
-    $('.js--wp-3').waypoint(function (direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, { offset: '50%' });
-
-    $('.js--wp-4').waypoint(function (direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, { offset: '50%' });
-
-
-    /!* Mobile navigation *!/
-
-    $('.js--nav-icon').click(function () {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.removeClass('ion-navicon-round').addClass('ion-close-round');
-        } else {
-            icon.removeClass('ion-close-round').addClass('ion-navicon-round');
-        }
-        nav.slideToggle(200);
-    });
-
-});*/
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+        || navigator.maxTouchPoints > 0
+        || navigator.msMaxTouchPoints > 0;
+}
 
 
 
