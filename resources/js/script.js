e7925dd76f3127a035fb0078604842f8b23b5acd
Mykhailo Kysliuk <misha.kysliuk@gmail.com>
2019-02-18 13:15:25 +0200

    Navigation rewritten with flex

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,12 +64,29 @@ $(document).ready(function () {
 
 
 
-document.querySelector('.mobile-nav-icon').addEventListener('click', function () {
-    var nav = document.querySelector('.navigation-list');
-    var icon = document.querySelector('.mobile-nav-icon i');
+document.querySelector('.navigation-mobile-icon').addEventListener('click', function () {
+    const nav = document.querySelector('.navigation-list');
+    const icon = document.querySelector('.navigation-mobile-icon i');
 
     icon.classList.toggle('ion-navicon-round');
     icon.classList.toggle('ion-close-round');
     nav.classList.toggle('in');
+});
 
+document.querySelectorAll('.link-dropdown-container').forEach(element => {
+    element.addEventListener('click', e => {
+        const navItem = e.target.closest('.navigation-item');
+        const dropdownLink = e.target.closest('.link-dropdown-container');
+
+        if (dropdownLink) {
+            const dropdownMenu = navItem.querySelector('.dropdown-menu');
+
+            if (dropdownMenu.clientHeight === 0) {
+                dropdownMenu.style.height = dropdownMenu.scrollHeight + 'px';
+            } else {
+                dropdownMenu.style.height = "0";
+            }
+            dropdownLink.classList.toggle('opened');
+        }
+    });
 });
